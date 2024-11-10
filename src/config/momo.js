c73a26b505c1b488674ede45f2524422c75ecbb5
tuananh <nguyntuananh1124@gmail.com>
2024-11-10 23:44:01 +0700

    TA update api

--- src/config/momo.js
+++ src/config/momo.js
@@ -4,7 +4,7 @@ module.exports = {
     MOMO_ACCESS_KEY: "F8BBA842ECF85",
     MOMO_SECRET_KEY: "K951B6PE1waDMi640xX08PD3vg6EkVlz",
     MOMO_ENDPOINT: "https://test-payment.momo.vn/v2/gateway/api/create",
-    REDIRECT_URL: "http://192.168.1.59:8081/momo-callback",
+    REDIRECT_URL: "http://localhost:8081/momo-callback",
     IPN_URL: "https://callback.url/notify",
     REQUEST_TYPE: "payWithMethod",
     MOMO_STATUS_ENDPOINT:"https://test-payment.momo.vn/v2/gateway/api/query",
