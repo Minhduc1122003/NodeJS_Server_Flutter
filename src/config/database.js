89f66d3d05d3aa535a68ceaf74e39b02740fa6ed
Minhduc1122003 <minhduc1122003@gmail.com>
2024-11-13 11:07:59 +0700

    mduc update deploy

--- src/config/database.js
+++ src/config/database.js
@@ -4,12 +4,13 @@ const sql = require('mssql');
 const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    server: process.env.HOST_NAME, 
+    server: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     database: process.env.DB_DATABASE,
     options: {
-        encrypt:false, // Sử dụng nếu bạn đang kết nối đến Azure SQL Database
-        trustServerCertificate: true // Sử dụng nếu bạn đang phát triển local
-    }
+        encrypt: true, 
+        trustServerCertificate: true 
+    } 
 };
 
 module.exports = config;
