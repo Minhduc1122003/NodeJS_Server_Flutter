949f103ba2640804e5a2429a1858f339eba5c25e
Minhduc1122003 <minhduc1122003@gmail.com>
2024-11-21 00:11:05 +0700

    mduc update firebase va up load img insertmovie

--- src/routes/web.js
+++ src/routes/web.js
@@ -7,14 +7,14 @@ const upload = multer({
   storage,
   limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
   fileFilter: (req, file, cb) => {
-    console.log(file.mimetype);  // In ra kiểu mime của file
-    if (file.mimetype.startsWith('image/')) {
-      cb(null, true);
-    } else {
-      cb(new Error('Invalid file type'), false);
-    }
-  } 
-  
+  console.log(file.mimetype);  // In ra kiểu mime của file
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true);
+  } else {
+    cb(new Error('Invalid file type'), false);
+  }
+}
+
 });
 const { getHomepage, 
     findByViewID,
